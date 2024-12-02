450a05ffe822f0962307b98c900e9a387b6f18d9
Justine Chua <justinechua77@gmail.com>
2024-12-02 13:01:14 +0800

    add

---
+++ script/decks.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const addButton = document.querySelector(".add-button");
+    const overlay = document.querySelector(".add-deck-overlay");
+    const cancelButton = document.getElementById("cancel");
+    const form = document.getElementById("overlay-form");
+
+    addButton.addEventListener("click", () => {
+        overlay.classList.remove("hide-overlay");
+        document.body.classList.add("no-scroll");
+    });
+
+    cancelButton.addEventListener("click", (e) => {
+        e.preventDefault();
+        overlay.classList.add("hide-overlay");
+        document.body.classList.remove("no-scroll");
+        form.reset();
+    });
+
+    overlay.addEventListener("click", (e) => {
+      if (e.target === overlay) {
+        overlay.classList.add("hide-overlay");
+        document.body.classList.remove("no-scroll");
+        form.reset();
+      }
+  });
+});
